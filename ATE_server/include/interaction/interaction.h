c9174c7702f071575f4c351931b8317bbc0c749a
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-05 18:37:28 +0300

    Add interaction interface

---
+++ ATE_server/include/interaction/interaction.h
@@ -0,0 +1,49 @@
+#ifndef ATE_SERVER_INTERACTION_INTERACTION_H_
+#define ATE_SERVER_INTERACTION_INTERACTION_H_
+
+#include <string>
+
+/**
+  * @brief Interface class for interaction with LVDS board
+  **/
+class Interaction {
+    public:
+
+    /**
+     * @brief Get video stream URI from the LVDS board.
+     *  It is supposed that the LVDS board is running a video
+     *  server that is capable of providing client with such an
+     *  identifier. For example, the LVDS board is running a RTSP server. 
+     **/
+    virtual const std::string & Stream() = 0;
+
+    /**
+     * @brief Taps screen (press + release) at x,y coordinates
+     * @param x x coordinate
+     * @param y y coordinate
+     **/
+    virtual void Tap(int x, int y) = 0;
+
+    /**
+     * @brief Presses screen at x,y coordinates
+     * @param x x coordinate
+     * @param y y coordinate
+     **/
+    virtual void Press(int x, int y) = 0;
+
+    /**
+     * @brief Releases screen at x,y coordinates
+     * @param x x coordinate
+     * @param y y coordinate
+     **/
+    virtual void Release(int x, int y) = 0;
+
+    /**
+     * @brief Drags mouse pointer to x,y coordinates
+     * @param x x coordinate
+     * @param y y coordinate
+     **/
+    virtual void Drag(int x, int y) = 0;
+};
+
+#endif // ATE_SERVER_INTERACTION_INTERACTION_H_
