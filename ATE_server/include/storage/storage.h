f0301aa0416c2251eaa3e0168f98329e82ab4677
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-08 15:34:42 +0300

    Add file for future storage interface

---
+++ ATE_server/include/storage/storage.h
@@ -0,0 +1,13 @@
+#ifndef ATE_SERVER_STORAGE_STORAGE_H_
+#define ATE_SERVER_STORAGE_STORAGE_H_
+
+namespace storage {
+/**
+  * @brief Interface class for storage containers
+  **/
+class Storage {
+
+};
+}
+
+#endif // ATE_SERVER_STORAGE_STORAGE_H_
