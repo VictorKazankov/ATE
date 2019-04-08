b37781285e5f1391ac67d71515d1fb26aef59675
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-08 15:24:47 +0300

    Add const qualifier to methods and params

--- ATE_server/include/interaction/interaction.h
+++ ATE_server/include/interaction/interaction.h
@@ -15,35 +15,35 @@ class Interaction {
      *  server that is capable of providing client with such an
      *  identifier. For example, the LVDS board is running a RTSP server. 
      **/
-    virtual const std::string & Stream() = 0;
+    virtual const std::string & Stream() const = 0;
 
     /**
      * @brief Taps screen (press + release) at x,y coordinates
      * @param x x coordinate
      * @param y y coordinate
      **/
-    virtual void Tap(int x, int y) = 0;
+    virtual void Tap(const int x, const int y) const = 0;
 
     /**
      * @brief Presses screen at x,y coordinates
      * @param x x coordinate
      * @param y y coordinate
      **/
-    virtual void Press(int x, int y) = 0;
+    virtual void Press(const int x, const int y) const = 0;
 
     /**
      * @brief Releases screen at x,y coordinates
      * @param x x coordinate
      * @param y y coordinate
      **/
-    virtual void Release(int x, int y) = 0;
+    virtual void Release(const int x, const int y) const = 0;
 
     /**
      * @brief Drags mouse pointer to x,y coordinates
      * @param x x coordinate
      * @param y y coordinate
      **/
-    virtual void Drag(int x, int y) = 0;
+    virtual void Drag(const int x, const int y) const = 0;
 };
 
 #endif // ATE_SERVER_INTERACTION_INTERACTION_H_
