5bb86e35b3548bb376479ffabebca69b2ade386a
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-08 15:25:43 +0300

    Add common coordinate defines

---
+++ ATE_server/include/utils/defines.h
@@ -0,0 +1,11 @@
+#ifndef ATE_SERVER_UTILS_DEFINES_H_
+#define ATE_SERVER_UTILS_DEFINES_H_
+
+namespace defines {
+
+#define x_y std::pair(int, int)
+#define x_y_w_h std::tuple<x_y, int, int>
+
+}
+
+#endif // ATE_SERVER_UTILS_DEFINES_H_
