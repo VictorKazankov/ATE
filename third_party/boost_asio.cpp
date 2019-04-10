a45aa1185f62ba709b8c587224cbd6f84a1c1e84
Serhii Muchychka <SMuchychka@luxoft.com>
2019-04-10 20:06:05 +0300

    Add CMake support and some submodules
    VHAT-226
    
    Added Boost.Asio with dependencies (System, Winapi, Config, Predef)
    for network and low-level I/O programming
    https://think-async.com
    
    Added inih for *.ini files parsing
    https://github.com/benhoyt/inih
    
    Added Pybind11 for operability between C++ and Python
    https://pybind11.readthedocs.io/en/stable/index.html

---
+++ third_party/boost_asio.cpp
@@ -0,0 +1,4 @@
+// This file is used to build a library
+// https://www.boost.org/doc/libs/1_70_0/doc/html/boost_asio/using.html#boost_asio.using.optional_separate_compilation
+#include <boost/asio/impl/src.hpp>
+#include <boost/asio/ssl/impl/src.hpp>
