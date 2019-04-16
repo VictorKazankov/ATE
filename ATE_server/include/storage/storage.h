6de7a469c01d6dc73d8d0c6ab4fa226c6adb60eb
m-shvaik <MShvaiko@luxoft.com>
2019-04-16 18:49:26 +0300

    Extend interface of class Storage
    VHAT-229
    
    - Add virtual method 'Connect'
    - Add virtual method 'ItemPath'

--- ATE_server/include/storage/storage.h
+++ ATE_server/include/storage/storage.h
@@ -1,11 +1,19 @@
 #ifndef ATE_SERVER_STORAGE_STORAGE_H_
 #define ATE_SERVER_STORAGE_STORAGE_H_
 
+#include <string>
+
 namespace storage {
 /**
  * @brief Interface class for storage containers
  **/
-class Storage {};
+class Storage {
+ public:
+  virtual void Connect(const std::string&) = 0;
+  virtual std::string ItemPath(const std::string&) const = 0;
+
+  virtual ~Storage() = default;
+};
 }  // namespace storage
 
 #endif  // ATE_SERVER_STORAGE_STORAGE_H_
