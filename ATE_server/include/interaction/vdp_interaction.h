398a90e77ae30be7456dc4081b0d2b83eb8f6896
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-18 12:29:52 +0300

    Add vdp interaction barebone class
    VHAT-227

--- ATE_server/include/interaction/vdp_interaction.h
+++ ATE_server/include/interaction/vdp_interaction.h
@@ -1,44 +1,45 @@
-#ifndef ATE_SERVER_INTERACTION_INTERACTION_H_
-#define ATE_SERVER_INTERACTION_INTERACTION_H_
+#ifndef ATE_SERVER_INTERACTION_VDP_INTERACTION_H_
+#define ATE_SERVER_INTERACTION_VDP_INTERACTION_H_
 
-#include <string>
+#include "interaction.h"
 
 namespace interaction {
 /**
  * @brief Interface class for interaction with LVDS board
  **/
-class Interaction {
+class VDPInteraction : public Interaction {
  public:
-  virtual ~Interaction() = default;
+  VDPInteraction();
+  ~VDPInteraction() override = default;
 
   /**
    * @brief Taps screen (press + release) at x,y coordinates
    * @param x x coordinate
    * @param y y coordinate
    **/
-  virtual void Tap(const int x, const int y) const = 0;
+  void Tap(const int x, const int y) const override;
 
   /**
    * @brief Presses screen at x,y coordinates
    * @param x x coordinate
    * @param y y coordinate
    **/
-  virtual void Press(const int x, const int y) const = 0;
+  void Press(const int x, const int y) const override;
 
   /**
    * @brief Releases screen at x,y coordinates
    * @param x x coordinate
    * @param y y coordinate
    **/
-  virtual void Release(const int x, const int y) const = 0;
+  void Release(const int x, const int y) const override;
 
   /**
    * @brief Drags mouse pointer to x,y coordinates
    * @param x x coordinate
    * @param y y coordinate
    **/
-  virtual void Drag(const int x, const int y) const = 0;
+  void Drag(const int x, const int y) const override;
 };
 }  // namespace interaction
 
-#endif  // ATE_SERVER_INTERACTION_INTERACTION_H_
+#endif  // ATE_SERVER_INTERACTION_VDP_INTERACTION_H_
---
+++ ATE_server/src/interaction/vdp_interaction.cpp
@@ -0,0 +1,23 @@
+#include "interaction/vdp_interaction.h"
+
+namespace interaction {
+
+  VDPInteraction::VDPInteraction() {
+  }
+
+  void VDPInteraction::Tap(const int /*x*/, const int /*y*/) const {
+      
+  }
+
+  void VDPInteraction::Press(const int /*x*/, const int /*y*/) const {
+      
+  }
+
+  void VDPInteraction::Release(const int /*x*/, const int /*y*/) const {
+
+  }
+
+  void VDPInteraction::Drag(const int /*x*/, const int /*y*/) const {
+
+  }
+}
