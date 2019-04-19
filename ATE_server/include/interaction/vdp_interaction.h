2a9d5780e91ed5882c679b70cf4b5ae59a9a0778
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-19 16:29:42 +0300

    Integrate https client to lvds interaction
    VHAT-227

--- ATE_server/include/interaction/vdp_interaction.h
+++ ATE_server/include/interaction/vdp_interaction.h
@@ -1,13 +1,19 @@
 #ifndef ATE_SERVER_INTERACTION_VDP_INTERACTION_H_
 #define ATE_SERVER_INTERACTION_VDP_INTERACTION_H_
 
+#include "https_client.h"
 #include "interaction.h"
+
+#include <memory>
 
 namespace interaction {
 /**
  * @brief Interface class for interaction with LVDS board
  **/
 class VDPInteraction : public Interaction {
+ private:
+  std::unique_ptr<HttpsClient> vdp_client_;
+
  public:
   VDPInteraction();
   ~VDPInteraction() override = default;
--- ATE_server/src/interaction/vdp_interaction.cpp
+++ ATE_server/src/interaction/vdp_interaction.cpp
@@ -1,23 +1,31 @@
 #include "interaction/vdp_interaction.h"
 
+#include "boost/asio.hpp"
+#include "boost/asio/ssl.hpp"
+
 namespace interaction {
 
-  VDPInteraction::VDPInteraction() {
-  }
+VDPInteraction::VDPInteraction() {
+  boost::asio::io_context io_context;
+  boost::asio::ip::tcp::resolver resolver(io_context);
 
-  void VDPInteraction::Tap(const int /*x*/, const int /*y*/) const {
-      
-  }
+  boost::asio::ip::basic_resolver_results<boost::asio::ip::tcp> endpoints =
+      resolver.resolve("172.31.239.130", "8444");  // TODO remove hardcode
 
-  void VDPInteraction::Press(const int /*x*/, const int /*y*/) const {
-      
-  }
+  boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
+  ctx.set_default_verify_paths();
 
-  void VDPInteraction::Release(const int /*x*/, const int /*y*/) const {
+  vdp_client_ = std::make_unique<HttpsClient>(io_context, ctx, endpoints);
 
-  }
-
-  void VDPInteraction::Drag(const int /*x*/, const int /*y*/) const {
-
-  }
+  io_context.run();
 }
+
+void VDPInteraction::Tap(const int /*x*/, const int /*y*/) const {}
+
+void VDPInteraction::Press(const int /*x*/, const int /*y*/) const {}
+
+void VDPInteraction::Release(const int /*x*/, const int /*y*/) const {}
+
+void VDPInteraction::Drag(const int /*x*/, const int /*y*/) const {}
+
+}  // namespace interaction
