5ead00b8b33533291f0002c84c69cd61e2f067eb
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-04-26 17:12:05 +0300

    Initialize JsonStorage at the ATE
    VHAT-229

--- ATE_server/src/ate.cpp
+++ ATE_server/src/ate.cpp
@@ -1,11 +1,20 @@
 #include "ate.h"
+#include "storage/json_storage.h"
 
 namespace {
 constexpr auto kConfigFile = VHAT_DATA_PATH "/config.ini";
+const std::string kDBSection = "DB";
+const std::string kPathOption = "Path";
+const std::string kCollectionModeOption = "CollectionMode";
 }  // namespace
 
-ATE::ATE() : config_{kConfigFile} {
-  // TODO
+ATE::ATE()
+    : config_{kConfigFile},
+      storage_{std::make_unique<storage::JsonStorage>(config_.GetString(kDBSection, kPathOption, ""),
+                                                      config_.GetString(kDBSection, kCollectionModeOption, ""))} {
+  if (!storage_->Connect()) {
+    exit(EXIT_FAILURE);
+  }
 }
 
 ATE::~ATE() { Close(); }
