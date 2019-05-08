bac5c15245421bc0a45d664ed7c6f02978ab2ead
ihaidaienko <ihaidaienko@luxoft.com>
2019-05-08 16:01:38 +0300

    Create architecture for test project
    VHAT-301
    
    - Add tests skeleton
    - Add allure reporting tool
    - Add tox

---
+++ functional_tests/api.py
@@ -0,0 +1,5 @@
+# TODO: add ATE import
+
+
+# def wait_for_object(object, timeout=1000):
+#     return waitForObject(object, timeout)
---
+++ functional_tests/conftest.py
@@ -0,0 +1,37 @@
+import configparser
+import logging
+
+import pytest
+from functional_tests import api
+
+
+@pytest.fixture(scope='session')
+def config():
+    configuration = configparser.ConfigParser()
+    configuration.read('test_config.ini')
+    return configuration
+
+
+# TODO: add attach to application import
+# @pytest.fixture(scope='session', autouse=True)
+# def app_connector(config):
+#     sync_config = config["sync"]
+#     sync = attachToApplication(
+#     sync_config['name'], host=sync_config['host'], port=sync_config['port'])
+#     try:
+#         yield sync
+#     finally:
+#         sync.detach()
+
+
+@pytest.fixture(scope='module')
+def driver():
+    return api
+
+
+@pytest.fixture(autouse=True)
+def logger():
+    logger = logging.getLogger('Test Log')
+    logger.setLevel(logging.INFO)
+
+    return logger
