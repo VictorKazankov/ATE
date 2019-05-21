5e5e09f42ca84ea8e3cd670899b69b3c02478e9d
Oleksii Kuchuk <OKuchuk@luxoft.com>
2019-05-21 18:25:52 +0300

    Update build scripts to accommodate build on LVDS
    VHAT-293
    
     - add optional 'lvds' parameter for setup-environment.sh which branches
       script execution:
      	- do not install OpenCV dependencies and build OpenCV
    	- do not update git submodules (temporarily blocked by
    	  non-functional git on board) and interact with ide
     - fallback to C++14 standard in cmake
     - make Threads::Threads boost dependency public
     - add 'wget' to tools dependencies (DevOps request)

--- infrastructure/setup-environment.sh
+++ infrastructure/setup-environment.sh
@@ -1,14 +1,33 @@
 ## Example for usage:
-## ./setup-environment.sh
+## ./setup-environment.sh [lvds]
+
+readonly LVDS_TARGET="lvds"
+readonly DEV_TARGET="dev"
+
+if [ $1 ]
+then
+  if [ $1 == $LVDS_TARGET ]
+  then
+    readonly TARGET=$LVDS_TARGET
+  else
+    echo "Unknown target: $1"
+    echo "Run with option 'lvds' for build on LVDS board or without any args for dev setup environment."
+    exit 1
+  fi
+else
+  readonly TARGET=$DEV_TARGET
+fi
+
+echo "Setting up VHAT build environment for: $TARGET"
 
 ## Update repositories
 sudo apt update
 
 ## Install packages
-readonly VHAT_DEV_TOOLS="g++-5 ninja-build cmake clang-format pkg-config"
+readonly VHAT_DEV_TOOLS="wget g++-5 ninja-build cmake clang-format pkg-config"
 readonly VHAT_USE_LIBS="libssl-dev libjsoncpp-dev"
 readonly IMAGE_FORMAT_LIBS="libpng-dev libjpeg-dev libtiff-dev"
-readonly OPENCV_USE_LIBS="libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev"
+if [ $TARGET != $LVDS_TARGET ]; then readonly OPENCV_USE_LIBS="libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev"; fi
 readonly TESSERACT_DEV_TOOLS="autoconf automake libtool"
 
 # List of the all necessary packages
@@ -19,16 +38,32 @@ sudo apt install -y $INSTALL_PACKAGES
 ## Go to the source root directory
 cd "$(dirname "$0")/.."
 
-## Download submodules
-git submodule init
-git submodule update
+if [ $TARGET != $LVDS_TARGET ]
+then
+  ## Download submodules
+  git submodule init
+  git submodule update
 
-## Copy Visual Studio Code tasks
-mkdir -pv .vscode
-cp infrastructure/ide_integration/vscode-linux-tasks.json .vscode/tasks.json
+  ## Copy Visual Studio Code tasks
+  mkdir -pv .vscode
+  cp infrastructure/ide_integration/vscode-linux-tasks.json .vscode/tasks.json
+else
+  echo -e "\nWARNING: due to some limitations, git submodules were not updated automatically."
+  echo "Please ensure you have manually init and updated submodules by running commands:"
+  echo "  git submodule init"
+  echo "  git submodule update"
+  read -n 1 -s -r -p "Press any key to continue..."
+fi
 
 # Build the required libraries
 cd infrastructure/build_dependencies
-sh ./opencv.sh
+if [ $TARGET != $LVDS_TARGET ]
+then
+  sh ./opencv.sh
+else
+  echo -e "\nWARNING: OpenCV is assumed to be installed from JetPack 3.3."
+  echo "Please see installation instructions: https://github.ford.com/IVIToolsTeam/VHAT_2.0/wiki/Set-up-LVDS-board-for-the-ATE-Server"
+  read -n 1 -s -r -p "Press any key to continue..."
+fi
 sh ./leptonica.sh
 sh ./tesseract.sh
