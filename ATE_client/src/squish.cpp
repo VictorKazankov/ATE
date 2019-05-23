30c0e200c41bb7c486d1dfa7afaf137d689490f2
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-05-23 16:48:49 +0300

    Add reading data from config file
    VHAT-236
    
    - inited ATEInteraction
    - inited DefaultWaitForObjectTimeout

--- ATE_client/src/squish.cpp
+++ ATE_client/src/squish.cpp
@@ -4,9 +4,20 @@
 #include "logger/logger.h"
 
 #include "ate_interaction.h"
+#include "common.h"
 #include "squish.h"
 
 using namespace squish;
+
+namespace {
+const std::string kBoardSection = "BOARD";
+const std::string kAddressOption = "Address";
+const std::string kPortOption = "Port";
+const std::string kTestSettingSection = "TEST_SETTINGS";
+const std::string kWaitForObjectTimeoutOption = "WaitForObjectTimeout";
+
+static int kDefaultWaitForObjectTimeoutInMs = 0;
+}  // namespace
 
 std::unique_ptr<interaction::ATEInteraction> ate_interaction;
 
@@ -14,13 +25,20 @@ ApplicationContext API::AttachToApplication(const std::string&) {
   logger::info("ApplicationContext AttachToApplication()");
   if (ate_interaction) {
     logger::warn("Warning ate_interaction already exist");
-    return ApplicationContext();  // TODO implement
+    return ApplicationContext();
   }
 
   boost::asio::io_context io_context;
 
+  const std::string config_file{VHAT_CLIENT_DATA_PATH "/config.ini"};
+  common::SetUp(config_file);
+  kDefaultWaitForObjectTimeoutInMs = common::Config().GetInt(kTestSettingSection, kWaitForObjectTimeoutOption, 0);
+
   try {
-    ate_interaction = std::make_unique<interaction::ATEInteraction>(io_context, "127.0.0.1", "5000");
+    ate_interaction = std::make_unique<interaction::ATEInteraction>(
+        io_context, common::Config().GetString(kBoardSection, kAddressOption, ""),
+        common::Config().GetString(kBoardSection, kPortOption, ""));
+
   } catch (const boost::system::system_error& boost_error) {
     logger::critical("boost system error: {} ({})", boost_error.what(), boost_error.code());
     exit(EXIT_FAILURE);
@@ -31,16 +49,16 @@ ApplicationContext API::AttachToApplication(const std::string&) {
 
   io_context.run();
 
-  return ApplicationContext();  // TODO implement
+  return ApplicationContext();
 }
 
 Object API::WaitForObject(const std::string& object_or_name) {
-  return WaitForObject(object_or_name, 0);  // TODO implement testSettings.waitForObjectTimeout
+  return WaitForObject(object_or_name, kDefaultWaitForObjectTimeoutInMs);
 }
 
 Object API::WaitForObject(const Object& object_or_name) {
-  return WaitForObject(object_or_name, 0);  // TODO implement testSettings.waitForObjectTimeout
-}  // namespace API
+  return WaitForObject(object_or_name, kDefaultWaitForObjectTimeoutInMs);
+}
 
 Object API::WaitForObject(const squish::Object& /*object_or_name*/, int /*timeout_msec*/) {
   logger::info("Object WaitForObject()");
