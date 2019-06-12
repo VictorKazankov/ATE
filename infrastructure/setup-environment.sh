97feb46ce29e85fb7eddce32029abfdd20039bdb
Oleksii Kuchuk <OKuchuk@luxoft.com>
2019-06-12 13:01:16 +0300

    Modify setup-environment script
    VHAT-371
    
    - detect target platform automatically based on system architecture:
      aarch64 means LVDS board
    - do perform git submodule operation inside script on LVDS board
      as there is not particular issue with board itself
    - remove any user interactions. Just leave a message to user that
      OpenCV must be installed from elsewhere
    - add python2.7-dev as a VHAT dependency

--- infrastructure/setup-environment.sh
+++ infrastructure/setup-environment.sh
@@ -1,21 +1,19 @@
 ## Example for usage:
-## ./setup-environment.sh [lvds]
+## ./setup-environment.sh
+##
+## The target platform is determined automatically based on a CPU architecture:
+## if running on aarch64 then it is LVDS board and this script must not install OpenCV.
+## For other platforms, OpenCV is built and installed.
 
 readonly LVDS_TARGET="lvds"
 readonly DEV_TARGET="dev"
 
-if [ $1 ]
+uname -p | grep -x -q aarch64 && readonly TARGET=$LVDS_TARGET || readonly TARGET=$DEV_TARGET
+
+if [ ! $TARGET ]
 then
-  if [ $1 == $LVDS_TARGET ]
-  then
-    readonly TARGET=$LVDS_TARGET
-  else
-    echo "Unknown target: $1"
-    echo "Run with option 'lvds' for build on LVDS board or without any args for dev setup environment."
-    exit 1
-  fi
-else
-  readonly TARGET=$DEV_TARGET
+  echo "Failed to detect target platfrom. Terminating."
+  exit 1
 fi
 
 echo "Setting up VHAT build environment for: $TARGET"
@@ -25,7 +23,7 @@ sudo apt update
 
 ## Install packages
 readonly VHAT_DEV_TOOLS="wget g++-5 ninja-build cmake clang-format autoconf automake libtool pkg-config"
-readonly VHAT_USE_LIBS="libssl-dev libjsoncpp-dev"
+readonly VHAT_USE_LIBS="libssl-dev libjsoncpp-dev python2.7-dev"
 readonly IMAGE_FORMAT_LIBS="zlib1g-dev libpng-dev libjpeg-dev libtiff-dev"
 if [ $TARGET != $LVDS_TARGET ]; then readonly OPENCV_USE_LIBS="libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev"; fi
 readonly TESSERACT_DEV_TOOLS="autoconf automake libtool"
@@ -38,33 +36,26 @@ sudo apt install -y $INSTALL_PACKAGES
 ## Go to the source root directory
 cd "$(dirname "$0")/.."
 
+## Download submodules
+git submodule init
+git submodule update
+
 if [ $TARGET != $LVDS_TARGET ]
 then
-  ## Download submodules
-  git submodule init
-  git submodule update
-
   ## Copy Visual Studio Code tasks
   mkdir -pv .vscode
   cp infrastructure/ide_integration/vscode-linux-tasks.json .vscode/tasks.json
-else
-  echo -e "\nWARNING: due to some limitations, git submodules were not updated automatically."
-  echo "Please ensure you have manually init and updated submodules by running commands:"
-  echo "  git submodule init"
-  echo "  git submodule update"
-  read -n 1 -s -r -p "Press any key to continue..."
 fi
 
 # Build the required libraries
 cd infrastructure/build_dependencies
-if [ $TARGET != $LVDS_TARGET ]
-then
-  sh ./opencv.sh
-else
-  echo -e "\nWARNING: OpenCV is assumed to be installed from JetPack 3.3."
-  echo "Please see installation instructions: https://github.ford.com/IVIToolsTeam/VHAT_2.0/wiki/Set-up-LVDS-board-for-the-ATE-Server"
-  read -n 1 -s -r -p "Press any key to continue..."
-fi
+if [ $TARGET != $LVDS_TARGET ] ; then sh ./opencv.sh; fi
 sh ./leptonica.sh
 sh ./tesseract-trained-data.sh
 cmake -P tesseract.cmake
+
+if [ $TARGET == $LVDS_TARGET ]
+then
+  echo -e "\nOpenCV is assumed to be installed from JetPack 3.3."
+  echo "Please see installation instructions: https://github.ford.com/IVIToolsTeam/VHAT_2.0/wiki/Set-up-LVDS-board-for-the-ATE-Server"
+fi
