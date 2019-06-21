1e35ad23b410d89e97b7b515bc989645caec947b
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-06-21 17:57:26 +0300

    Fix crash when ATE can`t find images from storage
    VHAT-391

--- ATE_server/src/video_streaming/matching/matcher.cpp
+++ ATE_server/src/video_streaming/matching/matcher.cpp
@@ -67,6 +67,12 @@ cv::Rect Matcher::MatchImage(const std::string& object_path) {
   }
 
   const cv::Mat pattern = cv::imread(object_path, cv::IMREAD_GRAYSCALE);
+
+  if (pattern.empty()) {
+    logger::error("[matcher] The image cannot be read: {}", object_path);
+    return cv::Rect{};
+  }
+
   if (pattern.rows > gray_screen_.rows || pattern.cols > gray_screen_.cols) {
     logger::error("Wrong pattern for image detection: pattern size: {}, screen size: {}", pattern.size,
                   gray_screen_.size);
