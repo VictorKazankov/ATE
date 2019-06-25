dddfba08ab6cf805f133270984a068b13a45bc47
m-shvaik <MShvaiko@luxoft.com>
2019-06-25 15:56:46 +0300

    Replace 'tessdata' by 'tessdata fast'
    VHAT-385

--- infrastructure/build_dependencies/tesseract-trained-data.sh
+++ infrastructure/build_dependencies/tesseract-trained-data.sh
@@ -6,7 +6,7 @@ mkdir -pv ../third_party/Tesseract
 cd ../third_party/Tesseract
 
 # Download trained data if necessary
-readonly TESSERACT_TRAINED_DATA_ENG_URL=https://github.com/tesseract-ocr/tessdata/raw/4.00/eng.traineddata
+readonly TESSERACT_TRAINED_DATA_ENG_URL=https://github.com/tesseract-ocr/tessdata_fast/raw/4.0.0/eng.traineddata
 readonly TESSERACT_TRAINED_DATA_ENG_FILENAME=`basename "$TESSERACT_TRAINED_DATA_ENG_URL"`
 wget -nc "$TESSERACT_TRAINED_DATA_ENG_URL"
 
