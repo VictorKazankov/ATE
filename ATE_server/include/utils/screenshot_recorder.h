e5ad42f2a5384d1b9f9b7a755803fbfe533eb4f9
KVGrygoriev <KVGrygoriev@luxoft.com>
2019-06-27 14:34:02 +0300

    Implement screenshot recorder class
    VHAT-395

---
+++ ATE_server/include/utils/screenshot_recorder.h
@@ -0,0 +1,53 @@
+#ifndef ATE_SERVER_UTILS_SCREENSHOT_RECORDER_H_
+#define ATE_SERVER_UTILS_SCREENSHOT_RECORDER_H_
+
+#include <opencv2/core/types.hpp>
+
+#include <experimental/filesystem>
+
+namespace utils {
+
+namespace fs = std::experimental::filesystem;
+
+/**
+ * @brief Class for recording screenshots
+ **/
+
+class ScreenshotRecorder {
+  bool storage_directory_available_ = false;
+  bool enable_saving_screenshots_ = false;
+  fs::path screenshots_store_dir_;
+
+ public:
+  /**
+   * @brief Constructor
+   * @param enable_saving_screenshots flag which turns on creation of screenshots while recognition
+   * @param screenshots_store_dir points out folder to store screenshots
+   **/
+  ScreenshotRecorder(bool enable_saving_screenshots, const std::string& screenshots_store_dir);
+
+  bool IsScreenshotSavingEnabled() const;
+
+  /**
+   * @brief Saves screenshot to file
+   * @param frame frame to save to file
+   * @return true if successful, otherwise - false
+   **/
+  bool SaveScreenshot(const cv::Mat& frame) const;
+
+  /**
+   * @brief Saves screenshot to file with highlight area
+   * @param frame frame to save to file
+   * @param highlight_area area to highlight at the frame
+   * @param hint suffix for screenshot name
+   * @return true if successful, otherwise - false
+   **/
+  bool SaveScreenshot(const cv::Mat& frame, const cv::Rect& highlight_area, const std::string& hint = "") const;
+
+ private:
+  void ProcessStorageDirectory();
+  fs::path GetFileName(const std::string& file_suffix = "") const;
+};
+}  // namespace utils
+
+#endif  // ATE_SERVER_UTILS_SCREENSHOT_RECORDER_H_
---
+++ ATE_server/src/utils/screenshot_recorder.cpp
@@ -0,0 +1,107 @@
+#include "utils/screenshot_recorder.h"
+
+#include <ctime>
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "logger/logger.h"
+
+namespace utils {
+
+std::string GetStringTimestamp() {
+  std::time_t t = std::time(nullptr);
+  char buf[20];
+
+  if (std::strftime(buf, sizeof(buf), "%F_%T", std::localtime(&t))) return buf;
+
+  logger::warn("[screenshot_recorder] Can't convert timestamp to string");
+  return std::string();
+}
+
+ScreenshotRecorder::ScreenshotRecorder(bool enable_saving_screenshots, const std::string& screenshots_store_dir)
+    : enable_saving_screenshots_(enable_saving_screenshots) {
+  screenshots_store_dir_ = VHAT_SERVER_STORAGE_DIR;
+  screenshots_store_dir_ /= screenshots_store_dir;
+
+  ProcessStorageDirectory();
+}
+
+void ScreenshotRecorder::ProcessStorageDirectory() {
+  std::error_code ec;
+
+  if (fs::exists(screenshots_store_dir_, ec)) {
+    storage_directory_available_ = true;
+  } else {
+    if (fs::create_directory(screenshots_store_dir_, ec)) {
+      logger::info("[screenshot_recorder] Directory for storage screenshots was created at {}", screenshots_store_dir_);
+      storage_directory_available_ = true;
+    } else {
+      logger::warn("[screenshot_recorder] Can't create directory for storage screenshots at {}. {}",
+                   screenshots_store_dir_, ec.message());
+      storage_directory_available_ = false;
+    }
+  }
+}
+
+fs::path ScreenshotRecorder::GetFileName(const std::string& file_suffix) const {
+  std::string file_name = GetStringTimestamp();
+
+  if (file_name.empty()) return fs::path();
+
+  if (!file_suffix.empty()) {
+    file_name += "_" + file_suffix;
+  }
+  file_name.append(".png");
+
+  fs::path store_file = screenshots_store_dir_;
+  store_file /= file_name;
+
+  return store_file;
+}
+
+bool ScreenshotRecorder::IsScreenshotSavingEnabled() const { return enable_saving_screenshots_; }
+
+bool ScreenshotRecorder::SaveScreenshot(const cv::Mat& frame) const {
+  if (!storage_directory_available_) {
+    logger::warn("[screenshot_recorder] Screenshot storage directory is unavailable");
+    return false;
+  }
+
+  fs::path store_file = GetFileName();
+
+  if (store_file.empty()) return false;
+
+  cv::imwrite(store_file.c_str(), frame);
+
+  logger::info("[screenshot_recorder] Screenshot was stored to {}", store_file);
+
+  return true;
+}
+
+bool ScreenshotRecorder::SaveScreenshot(const cv::Mat& frame, const cv::Rect& highlight_area,
+                                        const std::string& hint) const {
+  if (!storage_directory_available_) {
+    logger::warn("[screenshot_recorder] Screenshot storage directory is unavailable");
+    return false;
+  }
+
+  std::string file_suffix = (highlight_area.area() ? "detected" : "not-detected");
+  if (!hint.empty()) file_suffix.append("_" + hint);
+
+  fs::path store_file = GetFileName(file_suffix);
+
+  if (store_file.empty()) return false;
+
+  cv::Mat marked_image;
+  cvtColor(frame, marked_image, cv::COLOR_GRAY2BGR);
+
+  rectangle(marked_image, highlight_area, cv::Scalar(0, 255, 0), 2, 8, 0);
+
+  cv::imwrite(store_file.c_str(), marked_image);
+
+  logger::info("[screenshot_recorder] Screenshot was stored to {}", store_file);
+
+  return true;
+}
+}  // namespace utils
