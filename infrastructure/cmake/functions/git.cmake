79dc410c513f4f2437113578745c641f4f8944ca
m-shvaik <MShvaiko@luxoft.com>
2019-10-11 14:17:43 +0300

    Modify ATE version representation
    VHAT-621
    
    - example for release: 1.3.0-1
     1 - major version
     3 - minor version
     0 - version patch
     1 - release candidate version
    
    - example for non release: 1.3.0.1-<hash commit>
     1 - major version
     3 - minor version
     0 - version patch
     1 - commits since tag
     <hash commit> - hash

--- infrastructure/cmake/functions/git.cmake
+++ infrastructure/cmake/functions/git.cmake
@@ -15,12 +15,27 @@ function(git_get_commit_hash GIT_COMMIT_HASH)
 
 endfunction(git_get_commit_hash)
 
+# Parse git tag and extract release candidate version
+# Example of tag: v1.3.0-rc2
+# GIT_RC_VERSION - output variable
+# Example usage: get_rc_version(${GIT_TAG} GIT_RC_VERSION)
+function(get_rc_version GIT_TAG GIT_RC_VERSION)
+    string(REGEX MATCH "rc[0-9]*" TMP ${GIT_TAG})
+    if("${TMP}" STREQUAL "")
+        set(${GIT_RC_VERSION} ${RC_VERSION} PARENT_SCOPE)
+    else()
+        string(REGEX MATCH "[0-9]" RC_VERSION ${TMP})
+        set(${GIT_RC_VERSION} "-${RC_VERSION}" PARENT_SCOPE)
+    endif()
+    
+endfunction(get_rc_version)
+
 # Parse git tag and get git tag, major version and minor version
 # If not tag on repo - set default tag 1.0.0
 # Tag must contain version: v-1.0.1, release-1.8.1 ...
-# GIT_TAG, GIT_TAG_VERSION_MAJOR, GIT_TAG_VERSION_MINOR - output variables
-# Example usage: git_get_latest_tag(GIT_TAG GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR)
-function(git_get_latest_tag GIT_TAG GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR)
+# GIT_TAG, GIT_TAG_VERSION_MAJOR, GIT_TAG_VERSION_MINOR GIT_TAG_VERSION_PATCH - output variables
+# Example usage: git_get_latest_tag(GIT_TAG GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_TAG_VERSION_PATCH)
+function(git_get_latest_tag GIT_TAG GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_TAG_VERSION_PATCH)
     execute_process(
         COMMAND "${GIT_EXECUTABLE}" describe --tags --abbrev=0
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
@@ -32,20 +47,23 @@ function(git_get_latest_tag GIT_TAG GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR)
     ##
     ## Parse tag
     ##
-    string(REGEX MATCHALL "[0-9]+" VERSION_LIST ${TAG_FROM_GIT})
-
+    string(REGEX MATCH "[0-9].[0-9].[0-9]" TMP ${TAG_FROM_GIT}) 
+    string(REGEX MATCHALL "[0-9]" VERSION_LIST ${TMP}) 
     list(LENGTH VERSION_LIST VERSION_LIST_LENGTH)
 
-    if (${VERSION_LIST_LENGTH} LESS 2)
+    if (${VERSION_LIST_LENGTH} LESS 3)
         message(SEND_ERROR "Failed to parge git tag major and minor version.")
     endif ()
 
     list(GET VERSION_LIST 0 VERSION_MAJOR)
     list(GET VERSION_LIST 1 VERSION_MINOR)
+    list(GET VERSION_LIST 2 VERSION_PATCH)
 
     set(${GIT_TAG} ${TAG_FROM_GIT} PARENT_SCOPE)
     set(${GIT_TAG_VERSION_MAJOR} ${VERSION_MAJOR} PARENT_SCOPE)
     set(${GIT_TAG_VERSION_MINOR} ${VERSION_MINOR} PARENT_SCOPE)
+    set(${GIT_TAG_VERSION_PATCH} ${VERSION_PATCH} PARENT_SCOPE)
+
 endfunction(git_get_latest_tag)
 
 # Get count commit from last tag
@@ -97,14 +115,13 @@ endfunction(git_check_tags_exists)
 # If no tags are visible, return version 1.0 and overall number of commits
 # GIT_TAG_VERSION_MAJOR - major part of version
 # GIT_TAG_VERSION_MINOR - minor part of version
-# GIT_NUMBER_OF_COMMITS_SINCE_TAG - number commits since tag or overall
-# GIT_COMMIT_HASH - latest commit hash
-# Example usage: git_get_version(GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_NUMBER_OF_COMMITS_SINCE_TAG GIT_COMMIT_HASH)
-function(git_get_version GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_NUMBER_OF_COMMITS_SINCE_TAG GIT_COMMIT_HASH)
+# GIT_VERSION_SUFFIX - number commits since tag or overall
+# Example usage: git_get_version(GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_TAG_VERSION_PATCH GIT_VERSION_SUFFIX)
+function(git_get_version GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_TAG_VERSION_PATCH GIT_VERSION_SUFFIX)
     git_check_tags_exists(TAG_EXISTS)
 
     if (${TAG_EXISTS})
-        git_get_latest_tag(TAG VERSION_MAJOR VERSION_MINOR)
+        git_get_latest_tag(TAG VERSION_MAJOR VERSION_MINOR VERSION_PATCH)
         git_get_number_of_commits_since_tag(COMMITS_SINCE_TAG ${TAG})
 
         message(STATUS "git: Most recent visible tag: " ${TAG})
@@ -114,8 +131,9 @@ function(git_get_version GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_NUMBER_
 
         set(VERSION_MAJOR "1")
         set(VERSION_MINOR "0")
+        set(VERSION_PATCH "0")
 
-        message(STATUS "git: No git tag is visible. Will assume version 1.0")
+        message(STATUS "git: No git tag is visible. Will assume version 1.0.0")
         message(STATUS "git: Overall number of commits: " ${COMMITS_SINCE_TAG})
     endif()
 
@@ -124,6 +142,12 @@ function(git_get_version GIT_TAG_VERSION_MAJOR GIT_TAG_VERSION_MINOR GIT_NUMBER_
 
     set(${GIT_TAG_VERSION_MAJOR} ${VERSION_MAJOR} PARENT_SCOPE)
     set(${GIT_TAG_VERSION_MINOR} ${VERSION_MINOR} PARENT_SCOPE)
-    set(${GIT_NUMBER_OF_COMMITS_SINCE_TAG} ${COMMITS_SINCE_TAG} PARENT_SCOPE)
-    set(${GIT_COMMIT_HASH} ${COMMIT_HASH} PARENT_SCOPE)
+    set(${GIT_TAG_VERSION_PATCH} ${VERSION_PATCH} PARENT_SCOPE)
+
+    if(${COMMITS_SINCE_TAG} EQUAL 0)
+      get_rc_version(${TAG} GIT_RC_VERSION)
+      set(${GIT_VERSION_SUFFIX} ${GIT_RC_VERSION} PARENT_SCOPE)
+    else()
+      set(${GIT_VERSION_SUFFIX} ".${COMMITS_SINCE_TAG}-${COMMIT_HASH}" PARENT_SCOPE)
+    endif()
 endfunction(git_get_version)
