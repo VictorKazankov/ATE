c293f826576bb723e6a6e568e42cee93bdaf07db
Nguyen Thanh TU <nttu@luxoft.com>
2020-01-23 12:00:04 -0500

    Disable GPIO status reading temporarily
    VHAT-1329
    
    - In case a bug with LDVS board, gpio has been change it status on and off at random rate
    - This could lead to incorrect acctual video status
    - And make functional test fail

--- ATE_server/src/utils/video_status.cpp
+++ ATE_server/src/utils/video_status.cpp
@@ -9,6 +9,11 @@ GpioVideoStatus::GpioVideoStatus() : video_gpio_(GetEnvSettings(EnvSettings::kGp
 
 GpioVideoStatus::~GpioVideoStatus() {}
 
-bool GpioVideoStatus::GetVideoStatus() { return video_gpio_.GetStatus(); }
+/**
+ * TODO: Refactor reading GPIO status in case LVDS board had a bug and change the status on and of at radom time.
+ * The second question is, how it can help if LVDS give wrong value of GPIO.
+ * Disable this feature temporary.
+ */
+bool GpioVideoStatus::GetVideoStatus() { return true; }
 
 }  // namespace utils
