9b5f7559beb805ff87ea388170e253b505219536
izhadonov <izhadonov@luxoft.com>
2020-03-27 19:43:06 +0200

    Add ability to use sftp for func tests
    VHAT-1429
    
    * updated requirements.txt with scp lib dependancy to sftp connection
    * add SftpConnection class for performing file transfer which is using ssh_connection for getting ssh connection for transport

---
+++ functional_tests/utils/sftp_connection.py
@@ -0,0 +1,19 @@
+from scp import SCPClient
+from functional_tests.utils.ssh_connect import start
+
+
+class SftpConnection:
+
+    def __init__(self, ssh_connection=None):
+        self._ssh = ssh_connection if ssh_connection else start()
+        self._sftp = SCPClient(self._ssh.get_transport())
+
+    def __del__(self):
+        self._sftp.close()
+        self._ssh.close()
+
+    def get_file(self, source_file, target_file):
+        self._sftp.get(source_file, target_file)
+
+    def put_file(self, source_file, target_file):
+        self._sftp.put(source_file, target_file)
