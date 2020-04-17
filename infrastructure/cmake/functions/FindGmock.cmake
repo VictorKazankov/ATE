a085d566234f06e78d46aa5f816c6490cba68372
Oleksii Kuchuk <OKuchuk@luxoft.com>
2020-04-17 13:06:23 +0300

    Fix gmock linkage per build config with MSVC
    VHAT-1814
    
    Add a dummy way to select libraries with `d` suffix for Debug
    configuration, and without one for Release.

--- infrastructure/cmake/functions/FindGmock.cmake
+++ infrastructure/cmake/functions/FindGmock.cmake
@@ -9,26 +9,44 @@
 #    target_link_libraries(foo ${GMOCK_BOTH_LIBRARIES})
 #
 function(FindGmock)
-    find_library(GMOCK_LIBRARY
-        NAMES gmock
-        libgmock
-        PATH_SUFFIXES lib)
+    if (NOT MSVC)
+        find_library(GMOCK_LIBRARY
+            NAMES gmock
+            libgmock
+            PATH_SUFFIXES lib)
 
-    if (${GMOCK_LIBRARY} STREQUAL "GMOCK_LIBRARY-NOTFOUND")
-        message(FATAL_ERROR "Can't find gmock library")
+        if (${GMOCK_LIBRARY} STREQUAL "GMOCK_LIBRARY-NOTFOUND")
+            message(FATAL_ERROR "Can't find gmock library")
+        endif()
+
+        find_library(GMOCK_LIBRARY_MAIN
+            NAMES gmock_main
+            libgmock_main
+            PATH_SUFFIXES lib)
+
+        if (${GMOCK_LIBRARY_MAIN} STREQUAL "GMOCK_LIBRARY_MAIN-NOTFOUND")
+            message(FATAL_ERROR "Can't find gmock_main library")
+        endif()
+
+        set(GMOCK_LIBRARIES ${GMOCK_LIBRARY} CACHE INTERNAL "GMOCK_LIBRARIES")
+        set(GMOCK_MAIN_LIBRARIES ${GMOCK_LIBRARY_MAIN} CACHE INTERNAL "GMOCK_MAIN_LIBRARIES")
+    else()
+        find_library(GMOCK_LIB_D NAMES gmockd PATH_SUFFIXES lib)
+        find_library(GMOCK_LIB NAMES gmock PATH_SUFFIXES lib)
+        find_library(GMOCK_LIB_MAIN_D NAMES gmock_maind PATH_SUFFIXES lib)
+        find_library(GMOCK_LIB_MAIN NAMES gmock_main PATH_SUFFIXES lib)
+
+        if (${GMOCK_LIB_D} MATCHES ".*-NOTFOUND" OR ${GMOCK_LIB} MATCHES ".*-NOTFOUND")
+            message(FATAL_ERROR "Can't find gmock library")
+        endif()
+
+        if (${GMOCK_LIB_MAIN_D} MATCHES ".*-NOTFOUND" OR ${GMOCK_LIB_MAIN} MATCHES ".*-NOTFOUND")
+            message(FATAL_ERROR "Can't find gmock_main library")
+        endif()
+
+        set(GMOCK_LIBRARIES debug ${GMOCK_LIB_D} optimized ${GMOCK_LIB} CACHE INTERNAL "GMOCK_LIBRARIES")
+        set(GMOCK_MAIN_LIBRARIES debug ${GMOCK_LIB_MAIN_D} optimized ${GMOCK_LIB_MAIN} CACHE INTERNAL "GMOCK_MAIN_LIBRARIES")
     endif()
 
-    find_library(GMOCK_LIBRARY_MAIN
-        NAMES gmock_main
-        libgmock_main
-        PATH_SUFFIXES lib)
-
-    if (${GMOCK_LIBRARY_MAIN} STREQUAL "GMOCK_LIBRARY_MAIN-NOTFOUND")
-        message(FATAL_ERROR "Can't find gmock_main library")
-    endif()
-
-    set(GMOCK_LIBRARIES ${GMOCK_LIBRARY} CACHE INTERNAL "GMOCK_LIBRARIES")
-    set(GMOCK_MAIN_LIBRARIES ${GMOCK_LIBRARY_MAIN} CACHE INTERNAL "GMOCK_MAIN_LIBRARIES")
     set(GMOCK_BOTH_LIBRARIES ${GMOCK_LIBRARIES} ${GMOCK_MAIN_LIBRARIES} CACHE INTERNAL "GMOCK_BOTH_LIBRARIES")
-
 endfunction(FindGmock)
