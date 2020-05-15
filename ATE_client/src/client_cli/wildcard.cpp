496c2d54820ca915b328fcad90520440397803c0
Oleksii Kuchuk <OKuchuk@luxoft.com>
2020-05-15 19:33:22 +0300

    Fix missing CLI exception wrapper for Wildcard
    VHAT-1794
    
    Add missing wrapper from native to CLI exception into
    Wildcard::GetMatchObjects() mapper.

--- ATE_client/src/client_cli/wildcard.cpp
+++ ATE_client/src/client_cli/wildcard.cpp
@@ -1,5 +1,7 @@
 #include "wildcard.h"
 
+#include <exception>
+
 #include "converters_from_cli.h"
 #include "converters_to_cli.h"
 #include "squish/wildcard.h"
@@ -10,9 +12,11 @@ using namespace impl_detail;
 
 // clang-format off
 
-System::Collections::Generic::List<ATE::Object^>^ Wildcard::GetMatchObjects() {
+System::Collections::Generic::List<ATE::Object^>^ Wildcard::GetMatchObjects() try {
   auto w = FromCli(this);
   return ToCli(w.GetMatchObjects());
+} catch (const std::exception& e) {
+  throw Rethrow(e);
 }
 
 // clang-format on
